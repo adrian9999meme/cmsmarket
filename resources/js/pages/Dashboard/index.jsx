60756383a7c5625f99525aab1232f52bf83d6ae1
adrian9999meme <adrianhelgiu9999@outlook.com>
2026-03-09 11:09:08 -0700

    fix: remove subscribe modal in dashboard

--- resources/js/pages/Dashboard/index.jsx
+++ resources/js/pages/Dashboard/index.jsx
@@ -58,7 +58,6 @@ const reports = [
 const Dashboard = props => {
   document.title = "Dashboard";
   const [modal, setmodal] = useState(false);
-  const [subscribemodal, setSubscribemodal] = useState(false);
   const [periodData, setPeriodData] = useState([]);
   const [periodType, setPeriodType] = useState("Year");
 
@@ -74,12 +73,6 @@ const Dashboard = props => {
   );
 
   const { chartsData, timediffer, date } = useSelector(dashboardSelector);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setSubscribemodal(true);
-    }, 2000);
-  }, []);
 
   useEffect(() => {
     setPeriodData(chartsData);
@@ -138,58 +131,6 @@ const Dashboard = props => {
           </Row>
         </Container>
       </div>
-
-      {/* subscribe ModalHeader */}
-      <Modal
-        isOpen={subscribemodal}
-        role="dialog"
-        autoFocus={true}
-        centered
-        data-toggle="modal"
-        toggle={() => {
-          setSubscribemodal(!subscribemodal);
-        }}
-      >
-        <div>
-          <ModalHeader
-            className="border-bottom-0"
-            toggle={() => {
-              setSubscribemodal(!subscribemodal);
-            }}
-          ></ModalHeader>
-        </div>
-        <div className="modal-body">
-          <div className="text-center mb-4">
-            <div className="avatar-md mx-auto mb-4">
-              <div className="avatar-title bg-light  rounded-circle text-primary h1">
-                <i className="mdi mdi-email-open"></i>
-              </div>
-            </div>
-
-            <div className="row justify-content-center">
-              <div className="col-xl-10">
-                <h4 className="text-primary">Subscribe !</h4>
-                <p className="text-muted font-size-14 mb-4">
-                  Subscribe our newletter and get notification to stay update.
-                </p>
-
-                <div
-                  className="input-group rounded bg-light"
-                >
-                  <Input
-                    type="email"
-                    className="form-control bg-transparent border-0"
-                    placeholder="Enter Email address"
-                  />
-                  <Button color="primary" type="button" id="button-addon2">
-                    <i className="bx bxs-paper-plane"></i>
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Modal>
 
       <Modal
         isOpen={modal}
